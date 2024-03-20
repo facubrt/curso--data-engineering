af6da4749156bfe54a13c5035eb92f22b9123be9
Facundo Barreto <facubrt@gmail.com>
2024-03-19 21:03:48 -0300

    entregable 2. Funciones ETL y Subiendo datos a Redshift

--- pre-entregable/config.py
+++ pre-entregable/config.py
@@ -1,10 +1,32 @@
 import os
+import psycopg2
 from constants import START_DATE, END_DATE
-from dotenv import load_dotenv
 
-# CONSTRUCCIÓN DE URL Y CONSULTA DE API
-# API https://api.nasa.gov/
-# Near Earth Object Web Service
-load_dotenv()
-API_KEY = os.getenv('API_KEY')
-URL = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=' + START_DATE + '&end_date=' + END_DATE + '&api_key=' + API_KEY
+def get_api_url():
+  # API https://api.nasa.gov/ - Near Earth Object Web Service
+  API_KEY = os.getenv('API_KEY')
+  api_url = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=' + START_DATE + '&end_date=' + END_DATE + '&api_key=' + API_KEY
+  return api_url
+
+def connect_to_redshift():
+  # DB REDSHIFT
+  URL_DB = os.getenv("URL_DB")
+  NAME_DB = os.getenv("NAME_DB")
+  USER_DB =  os.getenv("USER_DB")
+  PASSWORD_DB = os.getenv("PASSWORD_DB")
+  PORT_DB = os.getenv("PORT_DB")
+
+  try:
+    # CONECTAR A LA BASE DE DATOS DE REDSHIFT
+    connection = psycopg2.connect(
+        host=URL_DB,
+        dbname=NAME_DB,
+        user=USER_DB,
+        password=PASSWORD_DB,
+        port=PORT_DB
+    )
+    print("Conexión con Redshift exitosa")
+    return connection
+  
+  except Exception as e:
+    print(e)
